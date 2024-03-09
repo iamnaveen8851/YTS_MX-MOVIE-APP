f98d21d7ef70dd4f3585c0a7c46995ca95436659
Naveen Kumar <nk24528.nk81@gmail.com>
2024-03-09 14:09:00 +0530

    working on navbar and frooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,96 @@
-function Footer(){
-    return(
-        <h1>Footer</h1>
-    )
+import { Flex, Text, Box, Center } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <>
+      <Center
+        bg="black"
+        w={{
+          base: "100%",
+          sm: "100%",
+          md: "100%",
+          lg: "100%",
+          xl: "100%",
+          "2xl": "100%",
+        }}
+        p={8}
+        display={"grid"}
+        justifyContent={"space-around"}
+        alignItems={"center"}
+        gap={"20px"}
+      >
+        <Center
+        //   border={"5px solid green"}
+          w={{
+            base: "100%%",
+            sm: "100%",
+            md: "100%",
+            lg: "100%",
+            xl: "100%",
+            "2xl": "100%",
+          }}
+          m="auto"
+          display={"flex"}
+          gap={{
+            base : "4px",
+            sm : "5px",
+            md : "9px",
+            lg: "12px",
+            xl : "14px",
+            "2xl" : "14px"
+
+          }}
+          fontSize={{
+            base: "6.8px",
+            sm: "10px",
+            md: "14.5px",
+            lg: "16px",
+            xl: "17px",
+            "2xl": "18px",
+          }}
+        >
+          <Text color={"white"}>YTS © 2011 - 2024 </Text>
+          <Link style={{ color: "gray" }}> Blog</Link>
+          <Text color={"white"}>-</Text>
+          <Link style={{ color: "gray" }}> DMCA</Link>
+          <Text color={"white"}>-</Text>
+          <Link style={{ color: "gray" }}> API</Link>
+          <Text color={"white"}>-</Text>
+          <Link style={{ color: "gray" }}> RSS</Link>
+          <Text color={"white"}>-</Text>
+          <Link style={{ color: "gray" }}> Contact</Link>
+          <Text color={"white"}>-</Text>
+          <Link style={{ color: "gray" }}> Browse Movies </Link>
+          <Text color={"white"}>-</Text>
+          <Link style={{ color: "gray" }}> Requests</Link>
+          <Text color={"white"}>-</Text>
+          <Link style={{ color: "gray" }}> Login</Link>
+          <Text color={"white"}>-</Text>
+          <Link style={{ color: "gray" }}> Language</Link>
+        </Center>
+
+        <Box>
+          <Text
+            fontSize={{
+              base: "10px",
+              sm: "12px",
+              md: "14px",
+              lg: "16px",
+              xl: "18px",
+              "2xl": "20px",
+            }}
+            textAlign={"center"}
+            color="white"
+          >
+            By using this site you agree to and accept our{" "}
+            <Link style={{ color: "gray" }}>User Agreement</Link>, which can be
+            read <Link style={{ color: "gray" }}>here.</Link>
+          </Text>
+        </Box>
+      </Center>
+    </>
+  );
 }
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,121 @@
-function Navbar(){
-    return(
-        <h1>Navbar</h1>
-    )
+import {
+  Flex,
+  Box,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Spacer,
+} from "@chakra-ui/react";
+
+import { SearchIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
+function Navbar() {
+  const LinkStyle = {
+    color: "grey",
+    fontWeight: "600",
+  };
+  return (
+    <>
+      <Flex
+        bg="black"
+        color="white"
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        p={{
+          base: "3%",
+          sm: "3%",
+          md: "3%",
+          lg: "2%",
+          xl: "2%",
+          "2xl": "2%",
+        }}
+        w={{
+          base: "100%",
+          sm: "100%",
+          md: "100%",
+          lg: "100%",
+          xl: "100%",
+          "2xl": "100%",
+        }}
+      >
+        <Box >
+          <Image w={{
+            base : "75%",
+            sm : "90%",
+            md : "98%",
+            lg :  "100%"
+        }} src="https://yts.mx/assets/images/website/logo-YTS.svg" />
+        </Box>
+
+        {/* for small and medium screen */}
+        <Box border="5px solid gold"
+        display={{
+            base :" flex",
+            sm : "flex",
+            md : "flex",
+            lg : "none",
+            xl : "none",
+            "2xl" : "none"
+
+        }}
+
+        w={{
+            base : "60%",
+            sm : "70%",
+            md : "70",
+           
+
+        }}
+
+        p={{
+            base : "3%",
+            sm : "3%",
+            md : "3%",
+            
+        }}
+        ></Box>
+
+
+        {/* For large screen  */}
+        <Box
+          display={{
+            base: "none",
+            sm: "none",
+            md: "none",
+            lg: "flex",
+            xl: "flex",
+            "2xl": "flex",
+          }}
+          p={2}
+          w={{
+            base: "70%", // 0px
+            sm: "70%", // ~480px. em is a relative unit and is dependant on the font size.
+            md: "70%", // ~768px
+            lg: "60%", // ~992px
+            xl: "60%", // ~1280px
+            "2xl": "96em", // ~1536px
+          }}
+          alignItems="center"
+          gap="20px"
+        >
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <SearchIcon color="gray.300" />
+            </InputLeftElement>
+            <Input type="tel" placeholder="Quick search" />
+          </InputGroup>
+
+          <Link style={LinkStyle}>Home</Link>
+          <Link style={{ fontWeight: "600", color: "lightgreen" }}>4K</Link>
+          <Link style={LinkStyle}>Trending</Link>
+          <Link style={LinkStyle}>BrowseMovies</Link>
+          <Link style={LinkStyle}>Login</Link>
+          <Link style={LinkStyle}>Register</Link>
+        </Box>
+      </Flex>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
